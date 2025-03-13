6471053664e1534209858df02b8f7183856e0c43
Lucas-Srz <suarez.lucas.pro@gmail.com>
2025-03-13 18:51:31 +0100

    JS | Ajout de la function 'isVictorious()'

--- script/morpion.js
+++ script/morpion.js
@@ -4,11 +4,11 @@ console.log("Entrée dans morpion.js");
  ***********************************************************************/
 var dom_textArea, dom_buttonArea, dom_buttons, dom_text, dom_btn_play;
 var hasWon, isPlayerOne;
-var gamesBoard;
+var gameBoard;
 
 const VIDE = "";
 const PLAYER_ONE = "O";
-const PLAYER_TOW = "X";
+const PLAYER_TWO = "X";
 
 /*********************************************
  *          Déclaration des fonction         *
@@ -50,14 +50,14 @@ function reset(){
     }
 
     // J'initialise le tableaux gameBoard qui represente le plateaux de jeu avec une valeur VIDE dans les 9 cellules
-    gamesBoard = new Array();
+    gameBoard = new Array();
     for (var i = 0; i < dom_buttons.length; i++) {
-        gamesBoard[i] = VIDE;
+        gameBoard[i] = VIDE;
     }
 
     // J'utilise le Array gameBoard pour affecter les bonnes valeurs dans le DOM (button.cell)
     for (var i = 0; i < dom_buttons.length; i++) {
-        dom_buttons[i].innerText = gamesBoard[i];
+        dom_buttons[i].innerText = gameBoard[i];
     }
 
     changeTextAndColor()
@@ -101,25 +101,104 @@ function clicCell(dom_self, index){
 
     // Je mets à jour gameBoard avec le symbole du joueur correspondant
     if (isPlayerOne) {
-        gamesBoard[index] = PLAYER_ONE;
+        gameBoard[index] = PLAYER_ONE;
     } else {
-        gamesBoard[index] = PLAYER_TOW;
+        gameBoard[index] = PLAYER_TWO;
     }
 
     // Avec le tableau JS (gameBoard), je mets à jour le plateau de jeu issu du DOM
-    dom_self.innerText = gamesBoard[index];
+    dom_self.innerText = gameBoard[index];
 
     // Je désactive le button qui à reçu le clic
     dom_self.disabled = true;
 
     // TODO : Faire la fonction !!!
-    // hasWon = isVictorious();
+    hasWon = isVictorious();
 
     // Si hasWon = true, alors je desactive tout les bouton.cell sinon, je change de joueur
     if (hasWon) {
+        for (var i = 0; i < dom_buttons.length; i++) {
+            dom_buttons[i].disabled = true;
+        }
+
+        // Je mets à jour le texte victorieux !! 
+        dom_text.innerText = "Félicitation,";
+        if (isPlayerOne) {
+            dom_text.innerText += " JOUEUR 1";
+        }
+        else{
+            dom_text.innerText += " JOUEUR 2";
+        }
+        dom_text.innerText += " a gagné !!!";
         
+        // Je vide la zone de texte
+        dom_textArea.textContent = "";
+
+        // Je mets à jour la zone de texte
+        dom_textArea.append(dom_text);
     } else {
         isPlayerOne = !isPlayerOne;
         changeTextAndColor();
+    }
+}
+
+/**
+ * Fonction isVictorious() vérifie si un joueur à gagné
+ * Elle retourne VRAI (a gagné) ou FAUX (n'a pas gagné) selon le cas de figure
+ */
+function isVictorious(){
+    // Je vérifie si c'est le tour de joueur 1
+    if(isPlayerOne){
+        if (
+            // Ligne 1
+            (gameBoard[0] == PLAYER_ONE && gameBoard[1] == PLAYER_ONE && gameBoard[2] == PLAYER_ONE) ||
+            // ligne 2
+            (gameBoard[3] == PLAYER_ONE && gameBoard[4] == PLAYER_ONE && gameBoard[5] == PLAYER_ONE) ||
+            // Ligne 3
+            (gameBoard[6] == PLAYER_ONE && gameBoard[7] == PLAYER_ONE && gameBoard[8] == PLAYER_ONE) ||
+            // Colonne 1
+            (gameBoard[0] == PLAYER_ONE && gameBoard[3] == PLAYER_ONE && gameBoard[6] == PLAYER_ONE) ||
+            // Colonne 2
+            (gameBoard[1] == PLAYER_ONE && gameBoard[4] == PLAYER_ONE && gameBoard[7] == PLAYER_ONE) ||
+            // Colonne 3
+            (gameBoard[2] == PLAYER_ONE && gameBoard[5] == PLAYER_ONE && gameBoard[8] == PLAYER_ONE) ||
+            // diagonale 1 (en haut à gauche vers en bas à droite)
+            (gameBoard[0] == PLAYER_ONE && gameBoard[4] == PLAYER_ONE && gameBoard[8] == PLAYER_ONE) ||
+            // diagonale 2 (en haut à droite vers en bas à gauche)
+            (gameBoard[2] == PLAYER_ONE && gameBoard[4] == PLAYER_ONE && gameBoard[6] == PLAYER_ONE)
+        ) 
+        {            
+            return true;
+        }
+        else{
+            return false
+        }
+    }
+    // Je vérifie si c'est le tour de joueur 2
+    else {
+        if (
+            // Ligne 1
+            (gameBoard[0] == PLAYER_TWO && gameBoard[1] == PLAYER_TWO && gameBoard[2] == PLAYER_TWO) ||
+            // ligne 2
+            (gameBoard[3] == PLAYER_TWO && gameBoard[4] == PLAYER_TWO && gameBoard[5] == PLAYER_TWO) ||
+            // Ligne 3
+            (gameBoard[6] == PLAYER_TWO && gameBoard[7] == PLAYER_TWO && gameBoard[8] == PLAYER_TWO) ||
+            // Colonne 1
+            (gameBoard[0] == PLAYER_TWO && gameBoard[3] == PLAYER_TWO && gameBoard[6] == PLAYER_TWO) ||
+            // Colonne 2
+            (gameBoard[1] == PLAYER_TWO && gameBoard[4] == PLAYER_TWO && gameBoard[7] == PLAYER_TWO) ||
+            // Colonne 3
+            (gameBoard[2] == PLAYER_TWO && gameBoard[5] == PLAYER_TWO && gameBoard[8] == PLAYER_TWO) ||
+            // diagonale 1 (en haut à gauche vers en bas à droite)
+            (gameBoard[0] == PLAYER_TWO && gameBoard[4] == PLAYER_TWO && gameBoard[8] == PLAYER_TWO) ||
+            // diagonale 2 (en haut à droite vers en bas à gauche)
+            (gameBoard[2] == PLAYER_TWO && gameBoard[4] == PLAYER_TWO && gameBoard[6] == PLAYER_TWO)
+        ) 
+        {
+            return true;
+        }
+        else{
+            return false
+        }
     }
 }
