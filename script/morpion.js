af3904644a6a3c2b6136d2cced40a7f03ba2bc13
Lucas-Srz <suarez.lucas.pro@gmail.com>
2025-03-13 14:08:45 +0100

    JS | Suppression des différentes function + MAJ de la function 'init()'

--- script/morpion.js
+++ script/morpion.js
@@ -20,81 +20,11 @@ const PLAYER_TOW = "X";
  */
 function init() {
     console.log("Entrée dans la fonction : init()");
-    // Déclaration des variables / constantes
-    let gamesBoard = new Array();
-    let hasWon = false;
-    let isPlayer1 = true;
+    // Récupère tous les éléments du DOM pour jouer une partie
+    dom_textArea = document.getElementById("textArea");
+    dom_buttons = document.getElementsByClassName("cell");
+    dom_btn_play = document.querySelector("#btnPlay");
 
-    const VIDE = " ";
-
-    // Initialiser le plateaux de jeux avec les valeurs de départ
-    initGamesBoard(gamesBoard,VIDE);
-
-    playOnce(gamesBoard);
-    console.log(gamesBoard);
-    
-    
-    
+    // Création d'une balise 'p' pour mettre à jour le #textArea en vonction du joueurs qui joue
+    dom_text = document.createElement("p");
 }
-
-/**
- * Fonction qui initialise le plateaux de jeux du morpion avec sa valeur "vide" dans les 9 cellules
- * @param {Array} plateauDeJeux 
- * @param {string} caracter
- */
-function initGamesBoard(plateauDeJeux, caracter){
-    console.log("Entrée dans la fonction initGamesBoard()");
-    const TAILLE = 9;
-
-    for (let i = 0; i < TAILLE; i++) {
-        plateauDeJeux[i] = caracter;
-    }
-}
-
-
-/**
- * Fonction qui manipule le DOM afin de rendre le plateaux de jeux clicables 
- */
-function playOnce() {
-    console.log("Fonction : playOnce()");
-    // Déclaration des variables pour le DOM
-    let dom_all_cell = document.getElementsByClassName("cell");
-    // Rendre le plateaux cliquable
-    for (let i = 0; i < dom_all_cell.length; i++) {
-        dom_all_cell[i].setAttribute("onclick", "detectCell(" + i + ")");
-        // console.log(i);
-    }
-
-}
-
-/**
- * TODO : Documentation à faire
- * @param {Number} index 
- */
-function detectCell(index){
-    console.log("Fonction : detectCell()s");
-    console.log(index);
-}
-
-// let newP = document.createElement("p");
-// let dom_textArea = document.getElementById("textArea");
-// let dom_all_p = document.getElementsByTagName("p");
-
-// Mettre à jour le texte
-// for (let i = dom_all_p.length - 1; i >= 0; i--) {
-//     dom_all_p[i].remove();
-// }
-// newP.innerText = "Joueurs 1, à toi de jouer !";
-// dom_textArea.appendChild(newP);
-// dom_textArea.setAttribute("class","player1");
-
-
-// Jouer un coup
-
-// Vérifier une victoire (Booléen)
-
-// Changer le texte du bouton "Jouer / Restart"
-
-// En cas de victoire, se faire plaisir en CSS
-
-// Alterner entre un thème bleu ou rouge selon le joueurs qui joue
